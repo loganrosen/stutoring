897a0e8173901be61c8fddd378909c6ba6075e73
Logan Rosen <loganrosen@gmail.com>
2014-09-27 01:18:06 -0400

    more stuff

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, session, redirect, url_for, escape, request, g
+import sqlite3
 
 app = Flask(__name__)
+
+
+DATABASE = './flaskr.db'
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
 
 @app.route('/')
 def index():
     if 'username' in session:
         return 'Logged in as %s' % escape(session['username'])
     return 'You are not logged in'
-    
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -22,6 +39,15 @@ def login():
     '''
 # route thingy (get or post
 def register():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+        <p><input type=text name=username>
+        <p><input type=submit value=Login/Register>
+        </form>
+        '''
     #registers
 
 @app.route('/logout')
